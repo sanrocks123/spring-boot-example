fc7cc784b4849f31733c1362d88c2beea0fb81f2
Sanjeev Saxena <sanrocks123@gmail.com>
2017-07-15 18:51:54 +0530

    addded test resources

---
+++ src/test/java/com/sanjeev/spring/boot/beans/HelloWorldImplTest.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2017. All Rights Reserved.
+ */
+package com.sanjeev.spring.boot.beans;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Sanjeev
+ *
+ */
+public class HelloWorldImplTest {
+
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+	}
+
+}
